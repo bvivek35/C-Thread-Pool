a60bf1751bdd7d8ce5eaba46f13b0e4c16bdab72
Vivek Bharadwaj <bvivek35@gmail.com>
2014-11-28 23:10:07 +0530

    added help in readme, made a TODO

--- configure.sh
+++ configure.sh
@@ -1,1 +1,5 @@
+# GitHub diesnot seem to host empty direcotries. 
+# This will setup those directories in your working directory.
+# There may be some other way, but I donot know at this point.
+
 mkdir -p lib obj 
